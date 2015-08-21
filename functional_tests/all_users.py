306b13cced497b81c26ec8c9d15a3dfb3470ea65
rvasquez <rvasquez@twitter.com>
2015-08-21 13:24:59 -0500

    Settings, static files and templates. FT and unit tests for static files and templates

--- functional_tests/all_users.py
+++ functional_tests/all_users.py
@@ -1,8 +1,10 @@
 from selenium import webdriver
-import unittest
+from django.core.urlresolvers import reverse
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
@@ -11,9 +13,16 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
-	def test_it_worked(self):
-		self.browser.get('http://localhost:8000')
-		self.assertIn('Welcome to Django', self.browser.title)
+	def get_full_url(self, namespace):
+		return self.live_server_url + reverse(namespace)
 
-if __name__ == '__main__':
-	unittest.main()
+	def test_home_title(self):
+		self.browser.get(self.get_full_url('home'))
+		self.assertIn('TaskBuster', self.browser.title)
+
+	def test_h1_css(self):
+		self.browser.get(self.get_full_url('home'))
+		h1 = self.browser.find_element_by_tag_name('h1')
+		self.assertEqual(h1.value_of_css_property('color'),
+			'rgba(200, 50, 255, 1)')
+
